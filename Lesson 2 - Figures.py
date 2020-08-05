2dfd2e9c25cd25fb9b561aa560623ccb243c11fe
Roberto Arevalo <roberttt44@hotmail.com>
2020-08-05 09:25:56 -0700

    2

---
+++ Lesson 2 - Figures.py
@@ -0,0 +1,12 @@
+#FCC-YT Lesson 2
+# printing a shape
+# order of instruction while using Python matters.
+print("    /|")
+print("   / |")
+print("  /  |")
+print(" /___|")
+
+print(" /___|")
+print("    /|")
+print("   / |")
+print("  /  |")
