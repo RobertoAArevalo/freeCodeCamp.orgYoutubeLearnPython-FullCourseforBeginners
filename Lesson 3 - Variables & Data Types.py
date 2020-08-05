f6491334dea449742f213c2e04342b27d88937e5
Roberto Arevalo <roberttt44@hotmail.com>
2020-08-05 09:58:56 -0700

    3

---
+++ Lesson 3 - Variables & Data Types.py
@@ -0,0 +1,38 @@
+#data can be diff. to manage to variables - which can be used as containers, can store certain data values
+#Variables help us manage our data inside of all of our programs.
+
+
+
+charcter_name = "Tom"
+character_age = "30"
+x = 10
+is_Male = False
+print("There once was a man named " + charcter_name + ",")
+print("He was " + character_age + " years old.")
+
+charcter_name = "Mike"
+print("He really liked the name " + charcter_name + ",")
+print("but didn't like being " + character_age + ".")
+
+# I can manually change the characters name and age.
+# I can create a variable to do this for me.
+# A charcter name needs to be created in order to use it as a variable.
+# In python separate words with an underscore.
+# If this variable is used with a changed named, it will use the newest name.
+
+#There are different type of data that can be stored inside of these variables
+#What is being store (i.e. "Tom") is a string.
+#A string is plain text.
+# "There once was a man named" is also a string.
+# There is a data type in python known as a string, and it is just the storage of plain text.
+# Another type of data that one can store are numbers.
+
+#When a number is being stored we do not need quotation marks.
+#Quotation marks are used for words.
+#Decimal numbers can also be used.
+
+#A Boolean value can also be stored. This is a true or false value.
+# There are many times where this will be useful.
+
+#Strings, Numbers, and True or False Values are basic types of data.
+
