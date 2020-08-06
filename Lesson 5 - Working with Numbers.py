19a38e111cffe28fffc77024567f5c12bb60e4a4
Roberto Arevalo <roberttt44@hotmail.com>
2020-08-05 21:06:34 -0700

    3

---
+++ Lesson 5 - Working with Numbers.py
@@ -0,0 +1,70 @@
+#Lesson 5 - Working with Numbers
+
+#numbers are a very common type of data type.
+
+#print(2)
+#   Will print the number
+#   Can also use PEMDAS
+
+# print(10 % 3)
+#   This is read 10 mod 3. This is taken the quotient and giving the remainder
+
+# my_num = 5
+# print(my_num)
+#   numbers can also be printed when declared as a variable and printing the respective variable
+
+# my_num = 5
+# print(str(my_num)
+#   This can be used to effectively to turing a variable that is a number into a string.
+# print(str(my_num) + "my favorite number")
+
+# my_num = -5
+# print(abs(my_num)
+    # using abs will provide the absolute value
+
+# my_num = -5
+# print(pow(3,2))
+    # pow allows me to pass or give 2 pieces of information.
+    # The function will have a number and the 2nd number will take the 1st number to the power of
+    # It will take 3^2
+
+#print(max(4,10))
+    # The max function will return the larger of the two numbers that have been passed into it.
+    # min will do the opposite.
+
+# print(round(3.2))
+    # The round function will round the number
+
+from math import*
+    # Importing can be done on Python. This is when external code is imported into our file.
+    # from math import* is importing this external file.
+    # This is a type of module. It is a math module with gives us access to more math functions.
+
+
+#print(floor(7.2))
+    #The floor function will round down avoiding math rules.
+
+#print(ceil(8.1))
+    #This function will round up avoiding math rules.
+
+#print(sqrt(25))
+    # the sqrt function will retrieve the square root of a number.
+
+my_num = -5
+print(sqrt(25))
+
+# Many of the math functions covered in this lesson are very commonly used.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
