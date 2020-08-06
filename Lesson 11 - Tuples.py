5c06e09feb06c748682ddc646657619d6d3eff5b
Roberto Arevalo <roberttt44@hotmail.com>
2020-08-06 05:10:19 -0700

    11

---
+++ Lesson 11 - Tuples.py
@@ -0,0 +1,18 @@
+#Lesson 11 - Tuples
+# A tuple is a type of data structure or a container where we can store different values.
+# A tuple is like a list. It is a structure where we can store many pieces of information.
+# There are key differences from lists however.
+# Acommon example of a tuple are cordinates
+# A tuple cannot be changed or modified
+
+# A list can be modified or changed but a tuple cannot be changed it is immutable.
+
+
+coordinates = [(4, 5), (6, 7), (80, 34)]
+coordinates[1] = 10
+print(coordinates[1])
+
+
+
+
+
