961ff4d18705be80af33b889034fdbfefd8c1085
Roberto Arevalo <roberttt44@hotmail.com>
2020-08-05 22:29:20 -0700

    6

---
+++ Lesson 6 - Getting Input From Users.py
@@ -0,0 +1,25 @@
+#Lesson 6 - Getting Input From Users
+
+# This tutorials shows how to get input from a user.
+# A user will be allowed to put information into the program
+# The information can then be stored inside of a variable and that variable can be used.
+
+#input()
+    #This function allows for getting information into
+
+# name = input("Enter your name: ")
+    # In this program name has turned into a variable.
+
+
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+print("Hello " + name + "! You are " + age)
+
+# Input from the user makes the program more interactive.
+# These inputs can be used for various things.
+
+
+
+
+
