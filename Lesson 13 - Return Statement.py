008b9a5363324cb8b61eba8c42c6b24d8cbca986
Roberto Arevalo <roberttt44@hotmail.com>
2020-08-07 02:29:05 -0700

    13

---
+++ Lesson 13 - Return Statement.py
@@ -0,0 +1,25 @@
+#Lesson 13 - Return Statement
+
+# collection of python code that performs a specific task.
+# We call on fucntions to perform it.
+# A function to cube a number
+# return is required to make the function work, and get information back from a function
+
+def cube(num):
+    return num*num*num # no code can be used after this return statement like
+    #print ("code") won't work here
+
+result = cube(4) #variable result will store value from the cube function
+print(result)
+
+# a string, bullion or array can be returned.
+
+
+
+
+
+
+
+
+
+
