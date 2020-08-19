35a98de2e92fe79bf4faba106cd529c92194ece3
Roberto Arevalo <roberttt44@hotmail.com>
2020-08-18 21:13:00 -0700

    28

---
+++ Lesson 29 - Classes and Objects.py
@@ -0,0 +1,22 @@
+# Lesson 29 - CLasses and Objects
+
+# I can create a phone data type to retain information about a phone.
+# With a class we can define a data type.
+
+# I can create a class for a student which will be like a student data type.
+# A class is an overview of what the student data type is
+# An object is an actual student with a name, etc.
+
+class Student:
+
+    def __init__(self, name, major, gpa, is on probation):  #inside this function I can map out what a student should have.
+        self.name = name
+        self.major = major
+        self.gpa = gpa
+        self.is_on_probation = is on probation
+
+
+
+
+
+
---
+++ app.py
@@ -0,0 +1,6 @@
+from Lesson 29 - Classes and Objects import Student
+# 1st student is from the file and the 2nd student is from the class
+student1 = Student("Jim", "Business", 3.1, False)
+#student1 is a student object
+
+print(student1.name)
