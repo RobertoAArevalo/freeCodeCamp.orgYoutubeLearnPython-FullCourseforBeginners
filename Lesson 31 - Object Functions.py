a69e4456704a07602aa9b9d7b06379c93ad7e487
Roberto Arevalo <roberttt44@hotmail.com>
2020-08-21 03:39:31 -0700

    31 and Student

---
+++ Lesson 31 - Object Functions.py
@@ -0,0 +1,14 @@
+#Lesson 31 - Object Functions
+
+from Student import Student
+
+student1 = Student("Oscar", "Accounting", 3.1)
+student2 = Student("Phyllis", "Business", 3.8)
+
+print(student2.on_honor_roll())
+#This is a class function that reports back true if the student has greater than a 3.5 in the "Student.py" file.
+#
+
+
+
+
